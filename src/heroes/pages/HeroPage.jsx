d8eac2c36a640eec5f768b4d4bb4c141df85b13f
Omar Castro <omarsc97@outlook.com>
2022-07-05 14:56:59 -0500

    Arreglar imagenes y mirar a partir del capitulo 198

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,9 +1,52 @@
-export const HeroPage = () => {
-  return (
-    <h1>
-    
-    Estas en HeroPage
+import { useMemo } from 'react';
+import { Navigate, useParams, useNavigate } from 'react-router-dom';
+import { getHeroeById } from './../helpers';
 
-    </h1>
+export const HeroPage = () => {
+
+  const { id } = useParams();
+
+  const navigate = useNavigate()
+  
+  const heroe = useMemo(() => getHeroeById ( id ), [id]);
+
+  const onNavigate = () => {
+    navigate(-1);
+  }
+
+  if ( !heroe ) {
+    return <Navigate to="/marvel"/>
+  }
+
+
+  return (
+    <div className='row mt-5'>
+      <div className="col-4">
+        <img 
+          src={`/assets/heroes/${id}.jpg`} 
+          alt={heroe.superhero} 
+          className='img-thumbnail'
+        />
+      </div>
+      <div className="col-8">
+        <h3> {heroe.superhero} </h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'> <b>Alet ego: </b> {heroe.alter_ego} </li>
+          <li className='list-group-item'> <b>First appereance: </b> {heroe.first_appearance} </li>
+          <li className='list-group-item'> <b>Publisher: </b> {heroe.publisher} </li>
+        </ul>
+        <h5 className='mt-3'> Characters </h5>
+        <p>{ heroe.characters }</p>
+
+        <button 
+          className='btn btn-outline-info'
+          onClick={onNavigate}
+        >Back</button>
+
+      </div>
+    
+    </div>
+
+    
   )
 }
